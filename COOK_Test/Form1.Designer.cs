149b857cb2a52c571e9a92617e38da84111f71f9
BenCollins97 <42169342+BenCollins97@users.noreply.github.com>
2020-03-25 12:04:01 +0000

    Add files via upload

---
+++ COOK_Test/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace COOK_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.postcodeTextBox = new System.Windows.Forms.TextBox();
+            this.deliveryTextBox = new System.Windows.Forms.TextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(301, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Postcode";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(279, 140);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Delivery Options";
+            // 
+            // postcodeTextBox
+            // 
+            this.postcodeTextBox.Location = new System.Drawing.Point(195, 43);
+            this.postcodeTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.postcodeTextBox.Name = "postcodeTextBox";
+            this.postcodeTextBox.Size = new System.Drawing.Size(262, 22);
+            this.postcodeTextBox.TabIndex = 2;
+            // 
+            // deliveryTextBox
+            // 
+            this.deliveryTextBox.Location = new System.Drawing.Point(150, 161);
+            this.deliveryTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.deliveryTextBox.Name = "deliveryTextBox";
+            this.deliveryTextBox.Size = new System.Drawing.Size(371, 22);
+            this.deliveryTextBox.TabIndex = 3;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(282, 88);
+            this.submitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(100, 28);
+            this.submitButton.TabIndex = 4;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 333);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.deliveryTextBox);
+            this.Controls.Add(this.postcodeTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox postcodeTextBox;
+        private System.Windows.Forms.TextBox deliveryTextBox;
+        private System.Windows.Forms.Button submitButton;
+    }
+}
+
---
+++ COOK_Test/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COOK_Test
+{
+    public partial class Form1 : Form
+
+    {
+        // associative array to simuulate database
+        string[] postcodes = { "TN9", "TN9 1AP", "TN8", "TN1", "TN11", "TN2", "TN10", "TN13", "TN14", "TN15", "ME", "ME10", "ME10 3", "IV" };
+        string[] deliveryOptions = { "Delivery from Warehouse", "No Deliveries", "Delivery from Warehouse", "Delivery from Warehouse", "Van Delivery, Collect from Tunbridge Wells", "Van Delivery, Collect from Tunbridge Wells", "Van Delivery", "Delivery from Sevenoaks, Collect from Sevenoaks", "Delivery from Sevenoaks, Collect from Sevenoaks", "Collect from Sevenoaks", "No Deliveries", "Collect from Kitchen", "Collect from Kitchen, Delivery from Sittingbourne", "No Deliveries" };
+
+        string currentOption = "";
+    
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void getData(string input)
+        {
+
+            currentOption = "Delivery By Courier";
+
+            for(int i = 0; i < postcodes.Length; i++)
+            {
+                if (input.ToUpper().Contains(postcodes[i]))
+                {
+                    currentOption = deliveryOptions[i];
+                }
+            }
+
+            deliveryTextBox.Text = currentOption;
+
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            getData(postcodeTextBox.Text);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
